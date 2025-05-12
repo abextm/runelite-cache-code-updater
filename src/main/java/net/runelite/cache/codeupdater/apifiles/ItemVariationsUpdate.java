63ed86821d702779cbed56449894694b04354f1e
Max Weber <mii7303@gmail.com>
2025-05-12 07:48:44 -0600

    do not generate variations for sandstone/granite

--- src/main/java/net/runelite/cache/codeupdater/apifiles/ItemVariationsUpdate.java
+++ src/main/java/net/runelite/cache/codeupdater/apifiles/ItemVariationsUpdate.java
@@ -47,6 +47,7 @@ public class ItemVariationsUpdate
 	private static String[][] replacements = new String[][]{
 		{"null", ""},
 		{"(clue|challenge) scroll \\(([^)]+)\\)", "clue scroll $2"},
+		{"(sandstone|granite) \\(([^)]+)\\)", "$1$2"},
 		{"\\([^)]+\\)", ""},
 		{"[^a-zA-Z0-9 +]", ""},
 		{" [0-9]+|[0-9]+ ", ""},
